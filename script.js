d35c4967d6a2e94b78d44081cc69688aebf9c805
Deepan <rdeepan326@gmail.com>
2025-03-05 11:07:38 +0530

    Local storage added

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ document.getElementById("btnAdd").addEventListener("click", function () {
     let inputText = document.getElementById("text").value; 
         let newItem = document.createElement("p");
         newItem.innerText = inputText; 
-        document.getElementById("box-container").appendChild(newItem); 
+      document.getElementById("box-container").appendChild(newItem); 
         document.getElementById("text").value = "";
-    
+       localStorage.setItem("val",document.getElementById("box-container").innerHTML);
 });
+const savedContent = localStorage.getItem("val");
+document.getElementById("box-container").innerHTML=savedContent;
+
 
 document.getElementById("btnRemove").addEventListener("click", function () {
     let boxContainer = document.getElementById("box-container");
